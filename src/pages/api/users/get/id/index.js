2b245170e131f4eead23c5782fa77aca3ab78450
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-10-08 17:32:37 -0500

    usersApiSlice 94839

---
+++ src/pages/api/users/get/id/index.js
@@ -0,0 +1,27 @@
+import handlerCors from "src/helpers/api/allowCors"
+
+import { getUsersApiAxios } from "src/apis/sanity/users"
+
+export default handlerCors().get(async (req, res) => {
+	const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
+
+	const queryUserById = `*[_type=="user"&&_id==$id][0]`
+	const _id = req.body.id
+
+	const queryParams = {
+		query: queryUserById,
+		$id: _id,
+	}
+
+	console.log(queryParams)
+
+	try {
+		const resp = await getUsersApiAxios.get(`/query/${dataset}`, {
+			params: queryParams,
+		})
+
+		res.send(resp.data.result)
+	} catch (error) {
+		res.status(401).send({ message: error })
+	}
+})
